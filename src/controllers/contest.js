098c49f1b633c3ccfb2f1dd8c8a1f016ed4cf9dc
kartheek542 <kartheekdivvela@gmail.com>
2025-03-06 22:00:01 +0530

    completed login and getcontests APIs

--- src/controllers/contest.js
+++ src/controllers/contest.js
@@ -4,7 +4,23 @@ import bcrypt from 'bcryptjs';
 
 export const getContests = async (req, res) => {
     try {
-        return res.status(200).json({ message: 'Hello Hostcode' });
+        const runningContestsQuery =
+            "select c.cid, c.name, c.start_time, c.end_time, json_agg(json_build_object('user_id', u.uid, 'username', u.username)) as authors from contest c inner join contest_author ca on c.cid = ca.contest_id inner join users u on ca.author_id = u.uid where c.start_time <= NOW() and c.end_time >= NOW() group by c.cid;";
+        const upcomingContestsQuery =
+            "select c.cid, c.name, c.start_time, c.end_time, json_agg(json_build_object('user_id', u.uid, 'username', u.username)) as authors from contest c inner join contest_author ca on c.cid = ca.contest_id inner join users u on ca.author_id = u.uid where c.start_time > NOW() group by c.cid order by c.start_time asc limit 5;";
+        const pastContestsQuery =
+            "select c.cid, c.name, c.start_time, c.end_time, json_agg(json_build_object('user_id', u.uid, 'username', u.username)) as authors from contest c inner join contest_author ca on c.cid = ca.contest_id inner join users u on ca.author_id = u.uid where c.end_time < NOW() group by c.cid order by c.end_time desc limit 5;";
+        const runningContestsResult = await db.query(runningContestsQuery, []);
+        const upcomingContestsResult = await db.query(upcomingContestsQuery, []);
+        const pastContestsResult = await db.query(pastContestsQuery, []);
+        return res.status(200).json({
+            contests: {
+                running: runningContestsResult.rows,
+                upcoming: upcomingContestsResult.rows,
+                recent: pastContestsResult.rows,
+            },
+            message: 'Successfully retrieved contests data',
+        });
     } catch (e) {
         console.log('Error occured while processing');
         console.log(e);
--- src/controllers/user.js
+++ src/controllers/user.js
@@ -38,9 +38,37 @@ export const registerUser = async (req, res) => {
 
 export const loginUser = async (req, res) => {
     try {
-        return res.status(200).json({ message: 'Hello Hostcode' });
+        const { euname, password } = req.body;
+        const userRecordResult = await db.query(
+            'select * from users where username=$1 or email=$2',
+            [euname, euname]
+        );
+        if (userRecordResult.rowCount === 1) {
+            const userRecord = userRecordResult.rows[0];
+            const isPasswordMatched = await bcrypt.compare(password, userRecord.password);
+            if (!isPasswordMatched) {
+                return res
+                    .status(401)
+                    .json({ message: 'Invalid username or password, Please try again' });
+            }
+            const jwtToken = jwt.sign(
+                {
+                    username: userRecord.username,
+                    user_id: userRecord.uid,
+                    email: userRecord.email,
+                },
+                process.env.JWT_SECRET
+            );
+            return res.status(200).json({
+                message: 'User logged in successfully',
+                hostcodeAccessToken: jwtToken,
+            });
+        } else {
+            return res.status(400).json({ message: 'Invalid username or password' });
+        }
     } catch (e) {
-        console.log('Error occured while processing');
+        console.log('Error occured while logging in user');
         console.log(e);
+        return res.status(500).json({ message: 'Error occurred while processing' });
     }
 };
